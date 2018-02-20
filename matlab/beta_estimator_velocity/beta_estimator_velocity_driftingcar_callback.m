16c75d18150b04353319e0446dae7c374569d443
Marco Baur <marco.baur@polimi.it>
2018-02-20 17:53:00 +0100

    updated matlab/beta_estimator_velocity_driftingcar.slx and added its callback function

---
+++ matlab/beta_estimator_velocity/beta_estimator_velocity_driftingcar_callback.m
@@ -0,0 +1,5 @@
+% BETA ESTIMATOR VELOCITY DRIFTINGCAR callback script to define Kpx,Kpy
+
+ptree=rosparam;
+Kpx=get(ptree,'beta_estimator_velocity_driftingcar/Kpx');
+Kpy=get(ptree,'beta_estimator_velocity_driftingcar/Kpy');
