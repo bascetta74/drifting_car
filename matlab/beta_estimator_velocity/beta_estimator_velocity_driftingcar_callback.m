c0658f14467f6467acc30f23cbded29ca5c619f3
Marco Baur <marco.baur@polimi.it>
2018-02-22 13:22:13 +0100

    WORKING vel beta estimator with its launch files and parameters files for ROS node

--- matlab/beta_estimator_velocity/beta_estimator_velocity_driftingcar_callback.m
+++
@@ -1,5 +0,0 @@
-% BETA ESTIMATOR VELOCITY DRIFTINGCAR callback script to define Kpx,Kpy
-
-ptree=rosparam;
-Kpx=get(ptree,'beta_estimator_velocity_driftingcar/Kpx');
-Kpy=get(ptree,'beta_estimator_velocity_driftingcar/Kpy');
