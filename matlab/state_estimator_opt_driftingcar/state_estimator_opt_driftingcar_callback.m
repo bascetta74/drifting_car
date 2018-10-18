ef5d56403f8674eea577ccff7eb3d50e734e1df5
Marco Baur <marco.baur@polimi.it>
2018-10-18 11:37:03 +0200

    updated state estimator simulink scheme to keep theta constant when there is a gap in the data optitrack

--- matlab/state_estimator_opt_driftingcar/state_estimator_opt_driftingcar_callback.m
+++ matlab/state_estimator_opt_driftingcar/state_estimator_opt_driftingcar_callback.m
@@ -1,3 +1,4 @@
-epsilon_ds=0.005; %[m] threshold below which ds is considered to be zero in the computation of V
+
+ds_nonzero_threshold=1e-3; %[m] threshold below which ds is considered to be zero
 
 theta_offset=5; %[rad] to compute theta (robot attitude angle) in the optitrack reference sys (i.e. 0 is when the robot is aligned with the X axis)
